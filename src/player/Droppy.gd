d46c0bc8b3f5f842887092a97a215a4998659d50
Mauro Sandoval <mgsandoval2021@gmail.com>
2025-09-22 14:37:02 -0300

    modificado el calculo de gota y agregado nivel de prueba

--- src/player/Droppy.gd
+++ src/player/Droppy.gd
@@ -5,16 +5,16 @@ class_name Droppy
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 var speed: float = 400 
-var jump_impulse: float = 700  # impulso de salto
+var jump_impulse: float = 700 
 var mass_scale_min: float = 1
 var mass_scale_max: float = 3.5
 var mass_min: float = 0.8
 var mass_max: float = 1.2
 var mass_scale_divisor: float = 0.9
-var temperature: float = 25
+var temperature: float = 12
 
 func _physics_process(delta: float) -> void:
-	# --- Movimiento horizontal ---
+	#Movimiento horizontal
 	var input_vector = Vector2.ZERO
 
 	if Input.is_action_pressed("right"):
@@ -26,15 +26,17 @@ func _physics_process(delta: float) -> void:
 		input_vector = input_vector.normalized() * speed
 		apply_central_force(input_vector)
 		
-	# --- Salto ---
+	#Salto
 	if Input.is_action_just_pressed("up"):
 		apply_impulse(Vector2(0, -jump_impulse))
 		
-	var scale_factor = remap(mass, mass_min, mass_max, mass_scale_min, mass_scale_max) #clamp(mass / mass_scale_divisor, mass_scale_min, mass_scale_max)
+	#escala el sprite y el collisionShape dependiendo la cantidad de masa
+	#el remap esta para que se exagere un poco mas el tamaño de la imagen en realacion con los valores de la masa
+	var scale_factor = remap(mass, mass_min, mass_max, mass_scale_min, mass_scale_max)
 	print(scale_factor)
 	sprite.scale = Vector2.ONE * scale_factor
 	collision_shape_2d.scale = sprite.scale
 
-
+# actualiza masa con la variacion calculada por el nivel
 func update_mass(variacion: float) -> void:
 	mass = clamp(mass + variacion, mass_min, mass_max)
