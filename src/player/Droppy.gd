65043f26d443a4feeb10a4b4aecbc76e950fc4de
Mauro Sandoval <mgsandoval2021@gmail.com>
2025-09-24 15:07:31 -0300

    agregado raycast

--- src/player/Droppy.gd
+++ src/player/Droppy.gd
@@ -3,6 +3,7 @@ class_name Droppy
 
 @onready var sprite = $Sprite2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var ray_cast_floot: RayCast2D = $RayCastFloot
 
 var speed: float = 250 
 var jump_impulse: float = 700 
@@ -13,9 +14,13 @@ var mass_max: float = 1.2
 var mass_scale_divisor: float = 0.9
 var temperature: float = 12
 
+
 func _physics_process(delta: float) -> void:
+	print("Raycast colisiona:", ray_cast_floot.is_colliding())
 	print(mass)
 	print({"temperaturaGota": temperature})
+	
+	ray_cast_floot.global_rotation = 0
 	#Movimiento horizontal
 	var input_vector = Vector2.ZERO
 
@@ -29,15 +34,17 @@ func _physics_process(delta: float) -> void:
 		apply_central_force(input_vector)
 		
 	#Salto
-	if Input.is_action_just_pressed("up"):
+	if Input.is_action_just_pressed("up") and ray_cast_floot.is_colliding():
 		apply_impulse(Vector2(0, -jump_impulse))
 		
+	
 	#escala el sprite y el collisionShape dependiendo la cantidad de masa
 	#el remap esta para que se exagere un poco mas el tamaño de la imagen en realacion con los valores de la masa
 	var scale_factor = remap(mass, mass_min, mass_max, mass_scale_min, mass_scale_max)
 	#print(scale_factor)
 	sprite.scale = Vector2.ONE * scale_factor
 	collision_shape_2d.scale = sprite.scale
+	ray_cast_floot.scale = sprite.scale
 
 # actualiza masa con la variacion calculada por el nivel
 func update_mass(variacion: float) -> void:
