659a33b8b8b543ca7b11f7706c7f1b96ea0080ed
Mauro Sandoval <mgsandoval2021@gmail.com>
2025-10-27 14:02:53 -0300

    avance de menu de inicio

--- Main.gd
+++ Main.gd
@@ -1,7 +1,19 @@
 extends Node
 
-func _ready() -> void:
-	Engine.time_scale = 0
+@export var level: PackedScene
 
-func _on_start_start_game() -> void:
-	Engine.time_scale = 1
+func _ready() -> void:
+	#Engine.time_scale = 0
+	pass
+
+#func _on_start_start_game() -> void:
+	#get_tree().change_scene_to_packed(level)
+	##Engine.time_scale = 1
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_packed(level)
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
