4ed839a95fd15a78d59172b93bbf1f8cdbd5068a
Mauro Sandoval <mgsandoval2021@gmail.com>
2025-10-27 16:52:03 -0300

    agregado pausa al juego

--- src/world/Nivel.gd
+++ src/world/Nivel.gd
@@ -70,3 +70,4 @@ func reset():
 	
 func pause():
 	$PauseMenu.show()
+	get_tree().paused = true
--- src/world/Pause_menu.gd
+++ src/world/Pause_menu.gd
@@ -7,12 +7,12 @@ func _ready() -> void:
 	self.hide()
 
 func _on_resume_button_pressed() -> void:
+	get_tree().paused = false
 	self.hide()
-	
 	
 
 func _on_main_menu_button_pressed() -> void:
-	get_tree().paused = false
+	#get_tree().paused = false
 	get_tree().change_scene_to_file(main_menu_scene_path)
 	
 
