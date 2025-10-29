627c56e76990915831c71acdef8cd753ba2cab8c
Mauro Sandoval <mgsandoval2021@gmail.com>
2025-10-29 12:11:11 -0300

    modificado el menu de victoria

---
+++ src/scenes/options_menu_victoria.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@export_file_path("*.tscn") var level_path: String
+@export_file_path("*.tscn") var main_menu_scene_path: String
+
+
+func _on_reset_button_pressed() -> void:
+	get_tree().change_scene_to_file(level_path)
+
+
+func _on_main_menu_button_pressed() -> void:
+	get_tree().change_scene_to_file(main_menu_scene_path)
+
+
+func _on_exit_button_pressed_victory() -> void:
+	get_tree().quit()
