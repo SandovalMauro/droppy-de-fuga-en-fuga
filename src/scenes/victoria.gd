1b0619ecf5267e45076745ddc37371c7ac59230a
Mauro Sandoval <mgsandoval2021@gmail.com>
2025-10-31 17:19:34 -0300

    arreglada pantalla de victoria

--- src/scenes/victoria.gd
+++ src/scenes/victoria.gd
@@ -1,12 +1,16 @@
-extends Control
+extends Node
 
-class_name Victoria
-signal restart
+@export_file_path("*.tscn") var level_path: String
+@export_file_path("*.tscn") var main_menu_scene_path: String
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file(level_path)
+
+
+func _on_main_menu_button_pressed() -> void:
+	get_tree().change_scene_to_file(main_menu_scene_path)
 
 
 func _on_exit_button_pressed() -> void:
-		get_tree().quit()
-
-
-func _on_restart_button_pressed() -> void:
-	restart.emit()
+	get_tree().quit()
